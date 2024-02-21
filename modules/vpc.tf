f80b34cccb80ea9a60256cb819a8020b522ce9c9
Krishnakanthk12 <160461689+Krishnakanthk12@users.noreply.github.com>
2024-02-21 18:14:37 +0530

    Update vpc.tf

--- modules/vpc.tf
+++ modules/vpc.tf
@@ -1,8 +1,3 @@
-# Configure the AWS Provider
-provider "aws" {
-  region = "ap-south-1"
-}
-
 resource "aws_vpc" "kubernetes_vpc" {
   cidr_block       = "10.0.0.0/16"
   instance_tenancy = "default"
